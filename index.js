28d566d75d9ea19a146cd9ac62011c26fdf3ad3d
flaviare1s <flaviarster@gmail.com>
2024-08-05 11:44:41 -0300

    Instalação das dependências e criação dos arquivos iniciais e do model contato.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import { config } from "dotenv"
+config()
+import mongoose from "mongoose"
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => {
+  console.log("Mongo DB conectado! ")
+}).catch((err) => {
+  console.log(err)
+})
+
+const app = express()
+app.use(express.json())
+
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000")
+})
