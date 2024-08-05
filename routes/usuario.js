834ce361993351cb7f5cb86200af3b91ded5951e
flaviare1s <flaviarster@gmail.com>
2024-08-05 17:23:18 -0300

    Adiciona restante do CRUD de usuários

--- routes/usuario.js
+++ routes/usuario.js
@@ -23,3 +23,51 @@ usuariosRouter.post('/usuarios', async (req, res) => {
     res.status(500).json({message: 'Um erro ocorreu ao criar o usuário'})
   }
 })
+
+usuariosRouter.get('/usuarios', async (req, res) => {
+  const lista = await Usuario.find()
+  res.json(lista)
+})
+
+usuariosRouter.get('/usuarios/:id', async (req, res) => {
+  const usuario = await Usuario.findById(req.params.id) 
+
+  if (usuario) {
+    res.json(usuario)
+  } else {  
+    res.status(404).json({message: 'Usário não encontrado'})
+  }
+})
+
+usuariosRouter.put('/usuarios/:id', async (req, res) => {
+  const { error, value } = usuarioValidation.validate(req.body, { abortEarly: false })
+
+  if (error) {
+    res.status(400).json({message: 'Dados inválidos', error: error.details})
+    return
+  }
+
+  const { nome, email, senha } = value
+
+  try {
+    const usuario = await Usuario.findByIdAndUpdate(req.params.id, {nome, email, senha})  
+
+    if (usuario) {
+      res.json({message: 'Usário atualizado com sucesso!'})
+    } else {
+      res.status(404).json({message: 'Usário não encontrado'})
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({message: 'Um erro ocorreu ao atualizar o usuário', error: err})
+  }
+})
+
+usuariosRouter.delete('/usuarios/:id', async (req, res) => {
+  const deletado = await Usuario.findByIdAndDelete(req.params.id)
+  if (deletado) {
+    res.json({message: 'Usário excluído com sucesso!'})
+  } else {
+    res.status(404).json({message: 'Usário não encontrado'})
+  }
+})
